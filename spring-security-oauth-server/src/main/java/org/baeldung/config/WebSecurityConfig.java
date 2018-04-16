51889e5bfdba9219236bdabf3802f41916a0d9cf
yang <yang@yang-PC>
2018-04-16 15:43:01 +0800

    sso use datasource

--- spring-security-oauth-server/src/main/java/org/baeldung/config/WebSecurityConfig.java
+++ spring-security-oauth-server/src/main/java/org/baeldung/config/WebSecurityConfig.java
@@ -1,11 +1,12 @@
 package org.baeldung.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,22 +14,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
+    
+	@Autowired
+	@Qualifier("ssoDataSource")
+	private DataSource dataSource;
+	
+  /*  @Autowired
     public void globalUserDetails(final AuthenticationManagerBuilder auth) throws Exception {
         // @formatter:off
+    
 	auth.inMemoryAuthentication()
 	  .withUser("john").password("123").roles("USER").and()
 	  .withUser("tom").password("111").roles("ADMIN").and()
 	  .withUser("user1").password("pass").roles("USER").and()
 	  .withUser("admin").password("nimda").roles("ADMIN");
+    
     }// @formatter:on
+    */
+    
+    @Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.jdbcAuthentication().dataSource(dataSource)
+		.usersByUsernameQuery("select username,password, enabled from users where username=?")
+		.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+    }
 
-    @Override
+   /* @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+    */
+  
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
